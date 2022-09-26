43c4d74d8f44798a02b7c0896b4910a2e5d80441
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-09-25 18:52:19 -0600

    adding response to create a README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('../utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt([
+            {type: 'input',
+            message: 'What is your GitHub URL?',
+            name: 'GitHub',
+            },
+        ])
+        .then((data) =>
+        fs.writeFile(`README.md`, JSON.stringify(data), (err) =>
+            err ? console.error(err) : console.log('Success!' + JSON.stringify(data)) 
+        )
+        )
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
